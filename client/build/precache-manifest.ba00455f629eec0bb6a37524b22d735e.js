e3c66f5e5f0e96d6b493e62653cd18181740202d
Thomas Brown <thobrown@microsoft.com>
2022-06-03 12:55:23 -0400

    removing build from git ignore

---
+++ client/build/precache-manifest.ba00455f629eec0bb6a37524b22d735e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c5c5f38f1599249079",
+    "url": "/static/css/main.a372dadd.chunk.css"
+  },
+  {
+    "revision": "45c5c5f38f1599249079",
+    "url": "/static/js/main.34634898.chunk.js"
+  },
+  {
+    "revision": "bf5e2ecf8749d6ce74dc",
+    "url": "/static/js/runtime~main.f1b3f998.js"
+  },
+  {
+    "revision": "f8ba715874d938dda6f9",
+    "url": "/static/css/2.c674b1e2.chunk.css"
+  },
+  {
+    "revision": "f8ba715874d938dda6f9",
+    "url": "/static/js/2.e08cc02a.chunk.js"
+  },
+  {
+    "revision": "b782c34f79543a617c3e",
+    "url": "/static/js/3.6927a301.chunk.js"
+  },
+  {
+    "revision": "00f55cec5697fc9acf8a",
+    "url": "/static/js/4.662fe98c.chunk.js"
+  },
+  {
+    "revision": "0ae9f13942251cdd4742",
+    "url": "/static/js/5.5532f298.chunk.js"
+  },
+  {
+    "revision": "d6ae32176b92c2fd029ed75b9801e8ce",
+    "url": "/static/media/credit_card.d6ae3217.png"
+  },
+  {
+    "revision": "b1764cbbdb98ed70ebf2e037203e18aa",
+    "url": "/static/media/profile_pic.b1764cbb.jpg"
+  },
+  {
+    "revision": "13ff7548e6f67daae3a4ef9df6d78f64",
+    "url": "/index.html"
+  }
+];
